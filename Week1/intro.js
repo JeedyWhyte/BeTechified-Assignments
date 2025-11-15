82d8acb657daf5b9e0021bb59fcab231af8ee5fa
JeedyWhyte <bryanaghogho78@gmail.com>
2025-11-15 17:05:28 +0100

    Week 1 and 2 API

---
+++ Week1/intro.js
@@ -0,0 +1,5 @@
+const name = "Jerry-Bassey Bryan Aghogho"
+const age = 18
+const favLan = "Phython"
+
+console.log("My name is " + name + ", I am " + age + " years old and my favorite programming language is " + favLan + ".");
---
+++ Week2/app.js
@@ -0,0 +1,36 @@
+require ('dotenv').config();
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.use(express.static('public'));
+
+const PORT = process.env.PORT
+
+//Custom Middleware
+app.use((req, res, next) => {
+    console.log(`[${req.method}] ${req.url} at ${new Date().toLocaleTimeString()}`);
+    next();
+})
+
+//For GET request
+app.get('/', (req, res) => res.send('My Week 2 API!'));
+
+//For POST request
+app.post('/user', (req, res) => {
+    const { name, email} =req.body;
+    //When field is missing
+    if (!name || !email) {
+        return res.status(400).json({ error: 'Missing required fields: name and email'});
+    }
+    res.send(`Hello, ${name}!`)
+})
+
+//For GET request with query parameters
+app.get('/user/:id', (req, res) => {
+    const id = req.params.id;
+    res.send (`User ${id} profile`)
+})
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
