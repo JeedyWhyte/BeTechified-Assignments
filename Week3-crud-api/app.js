5d2201c5e5fb3e8fdc7b2cb7408c12b023af7bd5
JeedyWhyte <bryanaghogho78@gmail.com>
2025-11-29 21:41:51 +0100

    Cors Added

--- Week3-crud-api/app.js
+++ Week3-crud-api/app.js
@@ -1,11 +1,14 @@
 require ('dotenv').config();
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 
 PORT = process.env.PORT
 
 // Middleware to parse JSON bodies
 app.use(express.json());
+app.use(cors('*'))
 
 let todos = [
     { id: 1, task: 'Learn Node.js', completed: false },
